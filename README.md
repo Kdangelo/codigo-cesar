afbccca39bae8fc54c9cb964af250bac7ebace07
Kdangelo <kdangelof@gmail.com>
2017-10-05 17:06:28 -0300

    solucion codigo cesar

---
+++ README.md
@@ -0,0 +1,5 @@
+##### ingreso datos
+##### lectura de string original
+##### ir a buscar cada letra de la linea de texto original
+##### escribir la letra correspondiente
+##### texto codificado
---
+++ app.js
@@ -0,0 +1,14 @@
+var str = prompt("ingresa la palabra a encriptar") // infreso str
+function encrypt (str, num) {  // funcion para encriptar str
+	for (var i = 0; i < str.length; i++) {
+		str[i]
+		if (srt.charCodeAt(0) >= 65 && str.charCodeAt(0) <= 90) { // devuelve el valor del unicode dela letra ingresada
+			 (x - 65 + x) % 65 + 65;
+		}if else {
+			str.charCodeAt >= 97 && str.charCodeAe <= 122
+		}
+	}
+	}
+
+
+}
